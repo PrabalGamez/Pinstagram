67ccde9e93c7f7c024d11963e4d2302f95ad428f
PrabalGamez <77341118+PrabalGamez@users.noreply.github.com>
2021-05-19 17:32:28 +0530

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,15 @@
+  var firebaseConfig = {
+    apiKey: "AIzaSyAu2XktOrJ-SAHajsLHCZeVu5pWy_7QzYs",
+    authDomain: "pinstagram-web.firebaseapp.com",
+    databaseURL: "https://pinstagram-web-default-rtdb.firebaseio.com",
+    projectId: "pinstagram-web",
+    storageBucket: "pinstagram-web.appspot.com",
+    messagingSenderId: "1004527038423",
+    appId: "1:1004527038423:web:96a2b3c704b45552ffa592",
+    measurementId: "G-BLW7NH8SML"
+  };
+  firebase.initializeApp(firebaseConfig);
+
+function save_name(){
+  window.alert(username);
+}
---
+++ kwitter_room.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+      apiKey: "AIzaSyBLw83ZXe5QAZwzd9_YCu_yJPulSBH7Rz4",
+      authDomain: "pinstagram-906dd.firebaseapp.com",
+      databaseURL: "https://pinstagram-906dd-default-rtdb.firebaseio.com",
+      projectId: "pinstagram-906dd",
+      storageBucket: "pinstagram-906dd.appspot.com",
+      messagingSenderId: "403134868458",
+      appId: "1:403134868458:web:30b841484c9d902350e093",
+      measurementId: "G-3VGEGZ37HK"
+    };
+    
+    firebase.initializeApp(firebaseConfig);
+
+    
